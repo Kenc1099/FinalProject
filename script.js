84dc94f71bc88fa9e666345e2389d1406a071a79
“Kenc1099” <kenc1099@gmail.com>
2018-06-19 16:21:56 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var library;
+var s1 = 0;
+
+
+ function checkout() {
+ 	library = 25
+	 var p = parseInt(prompt("How many books will you checkout?"));
+     s1 = s1 + p;
+     
+            document.getElementById("display").innerHTML = library - p;
+	 return document.getElementById("p1").innerHTML = s1;
+	console.log("hello")
+ }
+
+
+
+
