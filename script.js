2c33f095f21fb0edfbc5684d824d8498c168689c
“Kenc1099” <kenc1099@gmail.com>
2018-06-21 13:59:02 -0400

    working checkin button

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var library;
+var library = 25;
 var s1 = 0;
 
 
  function checkout() {
- 	library = 25
+
 	 var p = parseInt(prompt("How many books will you checkout?"));
      s1 = s1 + p;
-     
-            document.getElementById("display").innerHTML = library - p;
+     library = library - p;
+            document.getElementById("display").innerHTML = library;
 	 return document.getElementById("p1").innerHTML = s1;
 	console.log("hello")
  }
